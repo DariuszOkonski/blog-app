9a27ee114283a35e9ea6f8339fd25f20d74177b8
Dariusz Okoński <darek200180@gmail.com>
2025-06-30 19:53:12 +0200

    add Footer, Header and Navbar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import AddPost from './components/pages/AddPost/AddPost';
 import EditPost from './components/pages/EditPost/EditPost';
 import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
+import React from 'react';
+import Footer from './components/views/Footer/Footer';
+import Header from './components/views/Header/Header';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/post/:id' element={<Post />} />
-      <Route path='/post/add' element={<AddPost />} />
-      <Route path='/post/edit/:id' element={<EditPost />} />
-      <Route path='/about' element={<About />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <React.Fragment>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post/:id' element={<Post />} />
+        <Route path='/post/add' element={<AddPost />} />
+        <Route path='/post/edit/:id' element={<EditPost />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </React.Fragment>
   );
 }
 
