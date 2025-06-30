f0a50abebd4b83699a71b2f72f9e441fd4169489
Dariusz Okoński <darek200180@gmail.com>
2025-06-30 20:16:05 +0200

    add Footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,8 +1,14 @@
+import { Container } from 'react-bootstrap';
+
 function Footer() {
   return (
-    <div>
-      <h3>Footer</h3>
-    </div>
+    <Container>
+      <footer className='bg-white text-dark py-4 mt-5 rounded border'>
+        <div className='text-center'>
+          <p className='mb-0 text-muted'>Copyright &copy; BlogApp 2025</p>
+        </div>
+      </footer>
+    </Container>
   );
 }
 
