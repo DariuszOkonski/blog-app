2d94129bdcb3b6ffb9233f6ece63fdb55a1fe0e4
Dariusz Okoński <darek200180@gmail.com>
2025-07-01 20:06:04 +0200

    add Post component

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { getPost } from '../../../redux/postsRedux';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
+import PostCard from '../../common/PostCard/PostCard';
 
 function Post() {
   const { id } = useParams();
@@ -9,9 +11,35 @@ function Post() {
   console.log(post);
 
   return (
-    <div>
-      <h1>Post111</h1>
-    </div>
+    <Container>
+      <Row className='px-5'>
+        <Col md={6}>
+          <h4 className='text-dark text-sm-start mb-3'>{post.title}</h4>
+          <small className='text-muted'>
+            <strong>Author: </strong> {post.author}
+          </small>
+          <small className='text-muted'>
+            <strong>Published: </strong>
+            {post.publishedDate}
+          </small>
+          <p className='text-muted flex-grow-1'>
+            <small>{post.shortDescription}</small>
+          </p>
+        </Col>
+        <Col md={6}>
+          <div className='d-flex gap-2 justify-content-end'>
+            <Button
+              as={NavLink}
+              to={`/post/edit/${id}`}
+              variant='outline-success'
+            >
+              Edit
+            </Button>
+            <Button variant='outline-danger'>Delete</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
