d98061370f6f353e2ec2cfe8b1456d8df78a5ce3
Dariusz Okoński <darek200180@gmail.com>
2025-07-02 20:05:03 +0200

    refactor AddPost to use PostForm

--- src/components/pages/AddPost/AddPost.js
+++ src/components/pages/AddPost/AddPost.js
@@ -1,16 +1,24 @@
 import { Container } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { getPostsQuantity } from '../../../redux/postsRedux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addPost, getPostsQuantity } from '../../../redux/postsRedux';
 import SectionHeader from '../../common/SectionHeader/SectionHeader';
-import AddPostForm from '../../features/AddPostForm/AddPostForm';
+import PostForm from '../../features/PostForm/PostForm';
 
 function AddPost() {
   const postsQuantity = useSelector(getPostsQuantity);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (data) => {
+    dispatch(addPost(data));
+    navigate('/');
+  };
 
   return (
     <Container>
       <SectionHeader text={`Add Post Nr: ${postsQuantity + 1}`} />
-      <AddPostForm />
+      <PostForm action={handleSubmit} actionText='Add Post' />
     </Container>
   );
 }
